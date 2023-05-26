400ccdf62a5b3121d767d8ad67b0dbf8f2f5f903
To_Quy_Thanh <Toquythanh2k3@gmail.com>
2023-05-26 22:26:46 +0700

    feat: add html to index.php

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Super laptop</title>
+</head>
+<body>
+    <h1>Xin chào, thế giới!</h1>
+
+    <?php
+    // Mã PHP để hiển thị thông điệp "Xin chào, thế giới!"
+    echo "Xin chào, thế giới!";
+    ?>
+</body>
+</html>
