88eecf1becbb3554f4afcf87c24093b085f02d93
To_Quy_Thanh <Toquythanh2k3@gmail.com>
2023-05-28 00:40:37 +0700

    feat: support page

---
+++ templates/support/index.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Hỗ trợ</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            background-color: #f1f1f1;
+            padding: 20px;
+        }
+
+        .container {
+            max-width: 800px;
+            margin: 0 auto;
+            background-color: #fff;
+            padding: 30px;
+            border-radius: 5px;
+            box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
+        }
+
+        h1 {
+            text-align: center;
+            margin-bottom: 30px;
+        }
+
+        .support-item {
+            margin-bottom: 20px;
+            padding: 20px;
+            border: 1px solid #ccc;
+            border-radius: 4px;
+        }
+
+        .support-item-title {
+            font-weight: bold;
+            margin-bottom: 10px;
+        }
+
+        .support-item-description {
+            color: #888;
+        }
+
+        .support-action {
+            text-align: center;
+            margin-top: 20px;
+        }
+
+        .support-action a {
+            padding: 10px 20px;
+            background-color: #4caf50;
+            color: #fff;
+            text-decoration: none;
+            border-radius: 4px;
+        }
+    </style>
+</head>
+<body>
+    <div class="container">
+        <h1>Hỗ trợ</h1>
+
+        <div class="support-item">
+            <div class="support-item-title">Câu hỏi thường gặp</div>
+            <div class="support-item-description">Tìm câu trả lời cho những câu hỏi thường gặp của bạn.</div>
+        </div>
+
+        <div class="support-item">
+            <div class="support-item-title">Hướng dẫn sử dụng</div>
+            <div class="support-item-description">Tìm hiểu cách sử dụng sản phẩm và các tính năng của chúng.</div>
+        </div>
+
+        <div class="support-item">
+            <div class="support-item-title">Liên hệ hỗ trợ</div>
+            <div class="support-item-description">Liên hệ với đội ngũ hỗ trợ của chúng tôi để được giúp đỡ.</div>
+        </div>
+
+        <div class="support-action">
+            <a href="#">Xem tất cả hỗ trợ</a>
+        </div>
+    </div>
+</body>
+</html>
