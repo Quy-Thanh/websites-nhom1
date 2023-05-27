65e632ce4bdebee375132a388c0f26365538fe62
To_Quy_Thanh <Toquythanh2k3@gmail.com>
2023-05-28 00:42:36 +0700

    lib: install Faker lib

---
+++ lib/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'name/lib',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '1c35e8dfbf25fca68d3c923b1725f735c8005292',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'fzaninotto/faker' => array(
+            'pretty_version' => 'v1.5.0',
+            'version' => '1.5.0.0',
+            'reference' => 'd0190b156bcca848d401fb80f31f504f37141c8d',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../fzaninotto/faker',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'name/lib' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '1c35e8dfbf25fca68d3c923b1725f735c8005292',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
