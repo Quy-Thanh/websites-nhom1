5fa6420bc8e8990305c9eef65d3ca0bbefa33d25
To_Quy_Thanh <Toquythanh2k3@gmail.com>
2023-05-28 00:41:53 +0700

    feat: profile page

---
+++ templates/profile/account.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Profile</title>
+</head>
+<body>
+	<h2>Personal information</h2>
+	<p>This web uses this information to verify your identity and protect our community. You decide what personal information is visible to others.</p>
+	<form>
+		<table border="1">
+			<tbody>
+				<tr>
+					<td>Email</td>
+					<td><input type="text"></td>
+				</tr>
+				<tr>
+					<td>Phone</td>
+					<td><input type="text"></td>
+				</tr>
+				<tr>
+					<td>Username</td>
+					<td><input type="text"></td>
+				</tr>
+				<tr>
+					<td>Password</td>
+					<td><input type="text"></td>
+				</tr>
+				<tr>
+					<td>Confirm</td>
+					<td><input type="submit" value="change"></td>
+				</tr>
+			</tbody>
+		</table>
+	</form>
+	<a href="information.php">your information</a>
+</body>
+</html>
---
+++ templates/profile/information.php
@@ -0,0 +1,25 @@
+<h2>Personal information</h2>
+	<p>This web uses this information to verify your identity and protect our community. You decide what personal information is visible to others.</p>
+	<form>
+		<table border="1">
+			<tbody>
+				<tr>
+					<td>Name</td>
+					<td><input type="text"></td>
+				</tr>
+				<tr>
+					<td>Email</td>
+					<td><input type="text"></td>
+				</tr>
+				<tr>
+					<td>Phone</td>
+					<td><input type="text"></td>
+				</tr>
+				<tr>
+					<td>Address</td>
+					<td><input type="text"></td>
+				</tr>
+			</tbody>
+		</table>
+	</form>
+	<a href="index.php">your account</a>
